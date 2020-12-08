fbf5c30394c041291fc678f8990bce7e7000e188
Prince Ita <princeIta@outlook.com>
2020-12-08 22:57:40 +0100

    update doc

--- public/doc/api_project.js
+++ public/doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-08T21:31:34.216Z",
+    "time": "2020-12-08T21:57:15.841Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
