3828c43ed2fc009590a7631b1646b76cd5911ed1
Prince Ita <princeIta@outlook.com>
2020-12-09 16:02:35 +0100

    plugin sendgrid for email

--- src/gateways/email.js
+++ src/gateways/email.js
@@ -1,24 +1,48 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-	host: "smtp.ethereal.email",
-	port: 587,
-	secure: false,
-	auth: {
-		user: "larue.schmeler@ethereal.email",
-		pass: "u25gurE4Gnu4vzwT92",
-	},
-});
+// const transporter = nodemailer.createTransport({
+// 	host: "smtp.ethereal.email",
+// 	port: 587,
+// 	secure: false,
+// 	auth: {
+// 		user: "larue.schmeler@ethereal.email",
+// 		pass: "u25gurE4Gnu4vzwT92",
+// 	},
+// });
+
+const sgMail = require("@sendgrid/mail");
+const { EMAIL_API_KEY } = require("../config");
+
+sgMail.setApiKey(EMAIL_API_KEY);
 
 module.exports = {
 	send({ from, to, subject, text, html }) {
 		// send mail with defined transport object
-		transporter.sendMail({
-			from,
-			to,
-			subject,
-			text,
-			html,
-		});
+		// transporter.sendMail({
+		// 	from,
+		// 	to,
+		// 	subject,
+		// 	text,
+		// 	html,
+		// });
+
+		sgMail
+			.send({
+				from,
+				to,
+				subject,
+				text,
+				html,
+			})
+			.then(
+				() => {},
+				(error) => {
+					console.error(error);
+
+					if (error.response) {
+						console.error(error.response.body);
+					}
+				}
+			);
 	},
 };
