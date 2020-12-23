e0b0eca648179c3c9ddb4627788f56394a147feb
Prince Ita <princeIta@outlook.com>
2020-12-23 06:29:23 +0100

    update api docs for admin orders query

--- public/doc/api_project.js
+++ public/doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-22T07:40:55.914Z",
+    "time": "2020-12-23T05:28:54.839Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
