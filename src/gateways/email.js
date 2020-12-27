a4ad80120584781a64939aa3bdb5db50187f3a91
Prince Ita <princeIta@outlook.com>
2020-12-27 21:21:15 +0100

    get rid of unused comments

--- src/gateways/email.js
+++ src/gateways/email.js
@@ -1,15 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// const transporter = nodemailer.createTransport({
-// 	host: "smtp.ethereal.email",
-// 	port: 587,
-// 	secure: false,
-// 	auth: {
-// 		user: "larue.schmeler@ethereal.email",
-// 		pass: "u25gurE4Gnu4vzwT92",
-// 	},
-// });
-
 const sgMail = require("@sendgrid/mail");
 const { EMAIL_API_KEY } = require("../config");
 
@@ -17,15 +5,6 @@ sgMail.setApiKey(EMAIL_API_KEY);
 
 module.exports = {
 	send({ from, to, subject, text, html }) {
-		// send mail with defined transport object
-		// transporter.sendMail({
-		// 	from,
-		// 	to,
-		// 	subject,
-		// 	text,
-		// 	html,
-		// });
-
 		sgMail
 			.send({
 				from,
