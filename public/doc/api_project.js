823938fc6b0957c6ddc6e53f9097932533d976e4
Prince Ita <princeIta@outlook.com>
2021-01-16 13:37:03 +0100

    doc for password reset

--- public/doc/api_project.js
+++ public/doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-06T04:45:59.137Z",
+    "time": "2021-01-16T12:36:08.065Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
