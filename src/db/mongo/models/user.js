b5a28ec38fcb0c1847b87beaadbc9e2e7d3e45a6
Prince Ita <princeIta@outlook.com>
2021-03-09 07:09:21 +0100

    add rating order stats and truck to user model, to present deta in denormalized state for easy crud

--- src/db/mongo/models/user.js
+++ src/db/mongo/models/user.js
@@ -53,6 +53,15 @@ let schema = new Schema({
 	passwordResetCode: String,
 	passwordResetExpires: Date,
 	lastActive: Date,
+	rating: {
+		totalRating: Number,
+		ratingCount: Number,
+	},
+	orderStats: {
+		nCancelled: Number,
+		nCompleted: Number,
+		nOrders: Number,
+	},
 	products: [
 		{
 			productId: { type: Schema.Types.ObjectId, ref: "Product" },
@@ -64,6 +73,13 @@ let schema = new Schema({
 			quantity: Number,
 		},
 	],
+	truck: {
+		truckId: { type: Schema.Types.ObjectId, ref: "Truck" },
+		productId: { type: Schema.Types.ObjectId, ref: "Product" },
+		productName: String,
+		amount: Number,
+		quantity: Number,
+	},
 });
 
 schema.index({ latlon: "2dsphere" });
