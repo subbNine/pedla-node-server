d00937a956a4fc78b26d571d983dd82f62172e1e
Prince Ita <princeIta@outlook.com>
2021-03-18 14:51:47 +0100

    set default buyer type to regular buyer

--- src/entities/domain/buyer.js
+++ src/entities/domain/buyer.js
@@ -1,9 +1,10 @@
 const isType = require("../../lib/utils/is-type");
 const User = require("./user");
+const { buyerTypes } = require("../../db/mongo/enums/user");
 
 module.exports = class Buyer extends User {
 	corporateBuyerCacImg;
-	buyerType;
+	buyerType = buyerTypes.REGULAR;
 
 	constructor(fields = {}) {
 		super(fields);
