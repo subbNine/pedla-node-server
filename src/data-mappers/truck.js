afa04ac5a35fcff9a8e8e105177e5ddc29a36899
Prince Ita <princeIta@outlook.com>
2021-03-29 18:15:03 +0100

    update truck.js

--- src/data-mappers/truck.js
+++ src/data-mappers/truck.js
@@ -4,6 +4,7 @@ const {
 	ProductEnt,
 	PeddlerEnt,
 	DriverEnt,
+	PeddlerProductEnt,
 } = require("../entities/domain");
 const { isObjectId, isType } = require("../lib/utils");
 
@@ -19,7 +20,7 @@ module.exports = class TruckMapper extends BaseMapper {
 		const q = Truck.find(filter)
 			.populate("driverId")
 			.populate("ownerId")
-			.populate({ path: "productId" });
+			.populate("productId");
 
 		q.where({ $or: [{ isDeleted: false }, { isDeleted: { $exists: false } }] });
 
@@ -39,7 +40,7 @@ module.exports = class TruckMapper extends BaseMapper {
 		const q = Truck.findOne(this.toTruckPersistence(filter))
 			.populate("driverId")
 			.populate("ownerId")
-			.populate({ path: "productId" });
+			.populate("productId");
 
 		q.where({ $or: [{ isDeleted: false }, { isDeleted: { $exists: false } }] });
 
@@ -160,37 +161,38 @@ module.exports = class TruckMapper extends BaseMapper {
 			let driverEnt;
 			let productEnt;
 			if (isType("object", doc.ownerId) && !isObjectId(doc.ownerId)) {
-				truckOwnerEnt = this._toEntity(doc.ownerId, PeddlerEnt, {
+				truckOwnerEnt = doc.ownerId && this._toEntity(doc.ownerId, PeddlerEnt, {
 					_id: "id",
 					streetAddress: "address",
 				});
 			} else {
-				truckOwnerEnt = this._toEntity({ id: doc.ownerId }, PeddlerEnt, {
+				truckOwnerEnt = doc.ownerId && this._toEntity({ id: doc.ownerId }, PeddlerEnt, {
 					_id: "id",
 					streetAddress: "address",
 				});
 			}
 
 			if (isType("object", doc.driverId) && !isObjectId(doc.driverId)) {
-				driverEnt = this._toEntity(doc.driverId, DriverEnt, {
+				driverEnt = doc.driverId && this._toEntity(doc.driverId, DriverEnt, {
 					_id: "id",
 					streetAddress: "address",
 				});
 			} else {
-				driverEnt = this._toEntity({ id: doc.driverId }, DriverEnt, {
-					_id: "id",
-					streetAddress: "address",
-				});
+				driverEnt = doc.driverId
+					&& this._toEntity({ id: doc.driverId }, DriverEnt, {
+						_id: "id",
+						streetAddress: "address",
+					});
 			}
 
 			if (isType("object", doc.productId) && !isObjectId(doc.productId)) {
 				const entObj = doc.productId;
 
-				productEnt = this._toEntity(entObj, ProductEnt, {
+				productEnt = doc.productId && this._toEntity(entObj, ProductEnt, {
 					_id: "id",
 				});
 			} else {
-				productEnt = this._toEntity({ id: doc.productId }, ProductEnt, {
+				productEnt = doc.productId && this._toEntity({ id: doc.productId }, ProductEnt, {
 					_id: "id",
 				});
 			}
@@ -199,11 +201,14 @@ module.exports = class TruckMapper extends BaseMapper {
 				...doc,
 				owner: truckOwnerEnt,
 				driver: driverEnt,
-				product: {
-					product: productEnt,
-					...doc.productPrice,
-					id: productEnt.id
-				},
+				product: doc.productPrice && this._toEntity(
+					{
+						product: productEnt,
+						...doc.productPrice,
+						id: productEnt.id
+					}, PeddlerProductEnt,
+					{ _id: "id", productId: "product" }
+				),
 			};
 
 			const truckEnt = this._toEntity(entObj, TruckEnt, {
